520ba76056309b79b6b7b14442bbee1992cb6b5a
WereAM <michelle.ambunya@strathmore.edu>
2024-01-29 01:53:17 +0300

    feat(components): add Footer component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Components/Home';
 import React from 'react';
 import Projects from './Components/Projects';
 import Connect from './Components/Connect';
+import Footer from './Components/Footer';
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
     <Home />
     <Projects />
     <Connect />
+    <Footer />
     </>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css';
+
+export default function Footer() {
+    return(
+        <footer className="footer">
+                <p>Copyright &copy; Michelle 2024.</p>
+        </footer>
+    );
+}
