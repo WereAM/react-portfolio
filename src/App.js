9f7e9abfe1173b0496db498ae512ac34e511ebdb
WereAM <michelle.ambunya@strathmore.edu>
2024-01-29 01:27:08 +0300

    refactor: create Components folder to store components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './Components/Header';
+import Home from './Components/Home';
+import React from 'react';
 
 export default function App() {
   return (
     <>
     <Header/>
+    <Header />
+      <Home />
     </>
   );
 }
